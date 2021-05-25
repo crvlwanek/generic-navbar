6be6f9c304e7d9dbf1a72781a118af8cb2333326
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2021-05-25 12:25:57 -0500

    Initial commit

---
+++ src/nav/NavBar.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+
+const siteTitle = "Website Title";
+const leftMenu = ["Songs", "Albums", "Artists"];
+const rightMenu = ["Browse", "Contact", "Login"];
+
+export const NavBar: React.FC = () => {
+  let listener: any = null;
+  const [scrollState, setScrollState] = useState("top");
+
+  useEffect(() => {
+    listener = document.addEventListener("scroll", (e) => {
+      var scrolled: any | null = document.scrollingElement?.scrollTop;
+      if (scrolled >= 10) {
+        if (scrollState !== "amir") {
+          setScrollState("amir");
+        }
+      } else {
+        if (scrollState !== "top") {
+          setScrollState("top");
+        }
+      }
+    });
+    return () => {
+      document.removeEventListener("scroll", listener);
+    };
+  }, [scrollState]);
+
+  return (
+    <div
+      className={
+        "navbar__holder" +
+        " navbar__nav-" +
+        (scrollState === "top" ? "top" : "scroll")
+      }
+    >
+      <div className="navbar__wrapper">
+        <nav className="navbar__left-menu">
+          <ul className="navbar__list navbar__left-list">
+            {leftMenu.map((item) => (
+              <li className="navbar__list-item">
+                <button aria-label={item} className="navbar__list-link">
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="navbar__logo-wrapper">
+          <a className="navbar__logo-link" href="/">
+            <h1 className="navbar__logo">{siteTitle}</h1>
+          </a>
+        </div>
+        <nav className="navbar__right-menu">
+          <ul className="navbar__list navbar__right-list">
+            {rightMenu.map((item) => (
+              <li className="navbar__list-item">
+                <button aria-label={item} className="navbar__list-link">
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
